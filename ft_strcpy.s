673f371c641942d30475412c640d244e0e230ecc
Madasanya <margarete.hanna.mueller@gmail.com>
2024-10-22 00:24:23 +0200

    initital working strcpy

--- ft_strcpy.s
+++ ft_strcpy.s
@@ -1,18 +1,32 @@
 ; ----------------------------------------------------------------------------------------
-; ft_strcmp
-; 1. same length (just asking for first string sufficient)
-; 2. s1 is longer; termination with first occurence of unequal char
-; 3. s2 is longer; termination with first occurence of unequal char
+; ft_strcpy
 ; ----------------------------------------------------------------------------------------
+%macro  strlen 2
+    mov     %2, 0
+    jmp %%strlen_loop
+    %%strlen_inc: inc %2
+    %%strlen_loop:
+            cmp byte [%1+%2], 0
+            jne %%strlen_inc
+%endmacro
+
+%macro smaller 2
+    cmp %2, %1
+    cmovl %1, %2
+%endmacro
 
           global    ft_strcpy
           section   .text
-ft_strcpy:  mov rax, 0           ; init of return value with 0, also used as string index
-loop:       cmp rdi, 0
+ft_strcpy:  mov rax, rdi
+            strlen rdi, r8
+            strlen rsi, r9
+            smaller r8, r9
+loop:       cmp r8, 0
             je exit
-            cmp rsi, 0
-            je exit
-            mov rsi, rdi
-            inc rdi
-            inc rsi
+            mov dl, byte [rsi+r8]
+            mov byte [rdi+r8], dl
+            dec r8
+            dec rsi
+            dec rdi
+            jmp loop
 exit:       ret                          ; returns rax
