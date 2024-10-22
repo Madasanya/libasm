d78b1b239c137b4b731042e68438346515bff4ce
Madasanya <margarete.hanna.mueller@gmail.com>
2024-10-22 22:33:30 +0200

    fixed strcpy with stack usage

--- ft_strcpy.s
+++ ft_strcpy.s
@@ -1,31 +1,21 @@
 ; ----------------------------------------------------------------------------------------
 ; ft_strcpy
 ; ----------------------------------------------------------------------------------------
-%macro  strlen 2
-    mov     %2, 0
-    jmp %%strlen_loop
-    %%strlen_inc: inc %2
-    %%strlen_loop:
-            cmp byte [%1+%2], 0
-            jne %%strlen_inc
-%endmacro
-
-%macro smaller 2
-    cmp %2, %1
-    cmovl %1, %2
-%endmacro
 
           global    ft_strcpy
           section   .text
 ft_strcpy:  mov rax, rdi
-            strlen rdi, r8
-            strlen rsi, r9
-            smaller r8, r9
-loop:       cmp r8, 0
+src_end:    mov dl, byte [rsi]
+            push rdx
+            cmp byte [rsi], 0
+            je loop
+            inc rsi
+            inc rdi
+            jmp src_end
+loop:       pop rdx
+            mov byte [rdi], dl
+            cmp rax, rdi
             je exit
-            mov dl, byte [rsi+r8]
-            mov byte [rdi+r8], dl
-            dec r8
             dec rsi
             dec rdi
             jmp loop
