082d42e4793fd7dc7162c3362155731e68883cf3
Madasanya <margarete.hanna.mueller@gmail.com>
2024-10-30 11:15:14 +0100

    ft_list_size

--- src/ft_list_size.s
+++ src/ft_list_size.s
@@ -5,9 +5,11 @@
           global    ft_list_size
           section   .text
 ft_list_size: mov rax, 0
-loop:       cmp byte [rdi+8], 0
+            cmp rdi, 0x0
             je exit
-            inc rax
+loop:       inc rax
+            cmp byte [rdi+8], 0
+            je exit
             mov rdi, [rdi+8]
             jmp loop
 exit:       ret                           ; returns rax
