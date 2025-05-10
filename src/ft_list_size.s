4c9edfe3b7a8d935cd0e3574ca2fe31a0fd09a8b
Madasanya <margarete.hanna.mueller@gmail.com>
2025-05-10 17:48:56 +0200

    removed calle saved operations

--- src/ft_list_size.s
+++ src/ft_list_size.s
@@ -6,16 +6,15 @@
 
           global    ft_list_size
           section   .text
-ft_list_size: push rbp    ; Save the stack
-            mov  rbp, rsp
+ft_list_size:
             mov rax, 0
             cmp rdi, 0x0
             je exit
-loop:       inc rax
+loop:       
+            inc rax
             cmp byte [rdi+8], 0
             je exit
             mov rdi, [rdi+8]
             jmp loop
-exit:       mov rsp, rbp
-            pop rbp
+exit:       
             ret                          ; returns rax
